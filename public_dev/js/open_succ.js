1f48d7088341cc5a48e6d3b25b8a4722e67e62b1
Excel <linxy@fruitday.com>
2018-03-14 01:01:20 +0800

    error

--- public_dev/js/open_succ.js
+++ public_dev/js/open_succ.js
@@ -1,5 +1,5 @@
 var deviceId = Cookie.get("deviceId");
-// history.replaceState(null,null,'fruitday_index.html');//修改history.back
+history.replaceState(null,null,'index.html');//修改history.back
 (function() {
     common.checkLoginStatus(function() {
     })
@@ -19,7 +19,7 @@ function get_order_status() {
         }else if(response.status == "pay_succ"){
             location.href='buy_succ.html?order_name='+response.order_name+'&deviceId='+deviceId;//只显示关闭按钮
         }else if(response.status == "free"){
-
+            location.href='index.html';
         }
     }, function(e) {
         // Tools.showAlert(e.message || '服务器异常');
